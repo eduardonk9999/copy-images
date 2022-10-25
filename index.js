27b862111356ff171d3e7138f9e3cf46c2935dac
eduardonk9999 <thiago.novak7@gmail.com>
2022-10-25 18:45:44 -0300

    end script

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false
   });
+
   const page = await browser.newPage();
+  
   await page.goto('https://www.xn--frumnerd-v3a.com/2020/09/lista-toda-colecao-de-armaduras-do.html');
-  await page.evaluate(() => {
+  
+  const  imgList = await page.evaluate(() => {
+
     const nodeList = document.querySelectorAll('.item-post-padd img')
+
     const imgArray = [...nodeList]
 
 
-    const testeArray = imgArray.map(item => {
-      return item.currentSrc
-    })
+    const list = imgArray.map(item => (item.currentSrc))
 
-    console.log(testeArray)
+    return list
 
-   
-
-    // console.log(listImgs)
   })
-  // await browser.close();
+
+  fs.writeFile('iron.json', JSON.stringify(imgList, null, 2), err => {
+    if(err) throw new Error('deu ruim!!!')
+
+    console.log('NICE!!!')
+  })
+
+  await browser.close();
 })()
